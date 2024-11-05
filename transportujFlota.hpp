92e674bd67bd99cc03795640f4b5529a0469de57
Mr-Moose-dev <filip.lorin@gmail.com>
2024-11-05 08:37:23 +0100

    solved

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -5,5 +5,17 @@
 unsigned int transportujFlota(unsigned int towar)
 {
     // Twoja implementacja tutaj
-    return 0;
+    unsigned int suma = 0;
+    unsigned int liczba_zaglowcow = 0;
+    Stocznia stocznia{};
+    
+    while(suma < towar)
+    {
+        Statek* s = stocznia();
+        suma += s->transportuj();
+        if(dynamic_cast<Zaglowiec*>(s) != nullptr) {liczba_zaglowcow++;}
+        delete s;
+    }
+
+    return liczba_zaglowcow;
 }
